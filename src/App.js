e3468c61500dcc5b6f5091d97dc7e8b7a25af1ff
Jayden Rodriguez <02rodriguez.j@gmail.com>
2023-07-22 22:04:39 -0700

    Add inital basis to CSS and AppJS using styledJS:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import {StyledHeader} from './styled'
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
-const App = () => {
+function App(){
+
+
   return (
-    <>
+    <BrowserRouter>
+    <StyledHeader>
       <h1>Lambda Eats</h1>
       <p>You can remove this code and create your own header</p>
-    </>
+    </StyledHeader>
+    </BrowserRouter>
   );
 };
 export default App;
---
+++ src/styled.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const StyledHeader = styled.div`
+color: red;
+`
+
+export default {
+    StyledHeader
+}
